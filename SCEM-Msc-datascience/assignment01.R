1efaf1e1965fb74466e48a2e3b5206c130edecee
zliu15471 <zhen15471@gmail.com>
2022-10-03 01:04:10 +0100

    update

---
+++ SCEM-Msc-datascience/assignment01.R
@@ -0,0 +1,28 @@
+# 1. create a dataframe
+
+animals <- c("snake","Ostrich","Cat","Spider")
+animals
+
+num_legs <- c(0,2,4,8)
+num_legs
+
+animals_df <- data.frame(animals,num_legs)
+animals_df
+
+
+
+objects(pattern = "animals")
+
+?ls
+ls()
+ls(all.names = FALSE)
+ls(all.names = TRUE)
+ls(,pattern = "animals",all.names = TRUE)
+
+
+ls(pos =2,pattern = "animals_df",all.names = TRUE)
+
+
+?rm
+rm(num_legs)
+rm(list = ls())
